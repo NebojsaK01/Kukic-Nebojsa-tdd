a3be7e42943316cb591fa1943df9a5430a84d0f8
Nebojsa Kukic <nebojsakukic1@gmail.com>
2025-10-19 13:03:04 +0100

    Renamed the test cases appropriately.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,19 +3,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
-    void add() {
+    void testAdd() {
         Calculator c = new Calculator();
         assertEquals(10, c.add(7,3));
     }
 
     @Test
-    void subtract() {
+    void testSubtract() {
         Calculator c = new Calculator();
         assertEquals(5, c.subtract(12, 7));
     }
     @Test
-    void multiply() {
+    void testMultiply() {
         Calculator c = new Calculator();
         assertEquals(21, c.multiply(7, 3));
     }
+
+
 }
