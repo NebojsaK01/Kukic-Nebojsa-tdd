c1febc62cc50ca036ed39cb5931f4ddb5adca449
Nebojsa Kukic <nebojsakukic1@gmail.com>
2025-10-23 16:53:33 +0100

    This test should fail first but passes as the reserve() method exists from the skeleton code provided, albeit it's empty.

---
+++ src/test/java/library/ReservationServiceTest.java
@@ -0,0 +1,26 @@
+package library;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReservationServiceTest {
+
+    @Test
+    void userCanReserveIfCopiesAvailable() {
+
+        // Set up the repos and service.
+        IBookRepository bookRepo = new MemoryBookRepository();
+        IReservationRepository reservationRepo = new MemoryReservationRepository();
+        ReservationService service = new ReservationService(bookRepo, reservationRepo);
+
+        // Create book - id: 1, title: The Bible, copies Available: 10
+        Book book = new Book("1", "The Bible", 10);
+        bookRepo.save(book); // save the book
+
+
+        // reserve the book
+        service.reserve("Nebojsa",  "1");
+
+
+    }
+}
