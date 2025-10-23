0effb5a91341ae2187e05f9fad05b2e5726cd90e
Nebojsa Kukic <nebojsakukic1@gmail.com>
2025-10-23 17:39:13 +0100

    Test Failed - No validation in method for if copiesAvailable <= 0

--- src/test/java/library/ReservationServiceTest.java
+++ src/test/java/library/ReservationServiceTest.java
@@ -27,4 +27,22 @@ public class ReservationServiceTest {
         //check if the copies goes from 10 ->> 9
         assertEquals(9, book.getCopiesAvailable());
     }
+
+    @Test
+    void userCannotReserveIfCopiesNotAvailable() {
+
+        // Set up the repos and service.
+        IBookRepository bookRepo = new MemoryBookRepository();
+        IReservationRepository reservationRepo = new MemoryReservationRepository();
+        ReservationService service = new ReservationService(bookRepo, reservationRepo);
+
+        // Create book - id: 2, title: WW2 History, copies Available: 0
+        Book book = new Book("2", "WW2 History", 0);
+        bookRepo.save(book); // save the book
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> service.reserve("Nebojsa", "2"));
+
+        assertEquals("No copies available", exception.getMessage());
+    }
 }
